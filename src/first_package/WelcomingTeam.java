0862252514cfc1101f8f223c071e722e15a9570f
fcakgul <69127151+fcakgul@users.noreply.github.com>
2020-08-16 19:15:53 -0400

    Update WelcomingTeam.java
    3. commit from Fatma

--- src/first_package/WelcomingTeam.java
+++ src/first_package/WelcomingTeam.java
@@ -4,12 +4,12 @@ public class WelcomingTeam {
 
 	
 	public static void main(String[] args) {
+		System.out.println("Hey dilara");
+		System.out.println("Hello team 6, from Fatma -3. try");
 		
 		
 		
 		
-		
-		System.out.println("Ilk commit Said");
-		System.out.println("from deniz");
+		System.out.println("Ilk commit");
 	}
 }
