d19f6df50d8d9b06d61d1bfcb8f1d60fd882fc0e
Mesut Orhan <60322713+mesutor@users.noreply.github.com>
2020-08-16 19:19:30 -0400

    Update WelcomingTeam.java

--- src/first_package/WelcomingTeam.java
+++ src/first_package/WelcomingTeam.java
@@ -6,10 +6,10 @@ public class WelcomingTeam {
 	public static void main(String[] args) {
 		System.out.println("Hey dilara");
 		System.out.println("Hello team 6, from Fatma -3. try");
-		
-		
-		
-		
 		System.out.println("Ilk commit Said");
+		System.out.println("Mesut in mesut not master brach");
+		
+		
+		
 	}
 }
