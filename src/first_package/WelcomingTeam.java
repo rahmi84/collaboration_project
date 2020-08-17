06b830887f24c2d6d800fbc0f5ba28e596a6e2db
Rahmi <magicra84@gmail.com>
2020-08-16 21:18:17 -0300

    first from Rahmi

--- src/first_package/WelcomingTeam.java
+++ src/first_package/WelcomingTeam.java
@@ -6,9 +6,9 @@ public class WelcomingTeam {
 	public static void main(String[] args) {
 		System.out.println("Hey dilara");
 		System.out.println("Hello team 6, from Fatma -3. try");
-		
-		
-		
+
+
+		System.out.println("first trying from local Rahmi");
 		
 		System.out.println("Ilk commit Said");
 	}
